5c17a4453491882cb08f278640dab735618101c3
Fikri Haikal <fikrihaikal571@gmail.com>
2023-03-30 21:41:38 +0700

    rogo - Navigation Drawer

---
+++ app/src/main/java/com/example/latihan_navigationdrawer_rogos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.latihan_navigationdrawer_rogos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
